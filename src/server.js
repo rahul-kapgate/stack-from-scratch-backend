fb09f7c7a4f95e1ea53291c3dc416296ab505318
rahul-kapgate <rahulkapgateyt@gmail.com>
2025-12-04 12:15:55 +0530

    Deplyment on railways

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { ENV } from "./config/env.js";
 
-app.listen(ENV.PORT, () => {
+app.listen(ENV.PORT,"0.0.0.0", () => {
   console.log(`🚀 Server listening on port ${ENV.PORT} (${ENV.NODE_ENV})`);
 });
 
